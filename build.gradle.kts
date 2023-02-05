c259add6770de6dd330ac6c39efa9bd0d44ba797
Maxim <syromolotov.maks@mail.ru>
2023-02-05 17:35:17 +0300

    refactoring of build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("io.gitlab.arturbosch.detekt") version "1.14.0"
     application
 }
 
@@ -11,18 +13,42 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven(url = "https://dl.bintray.com/kotest/kotest")
+    maven(url = "https://plugins.gradle.org/m2/")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.72")
-    implementation("io.reactivex.rxjava2:rxjava:2.2.20")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.3.7")
-    testImplementation("io.kotest:kotest-assertions-core:4.2.2")
-    testImplementation("io.kotest:kotest-runner-junit5:4.2.2")
-    testImplementation("junit:junit:4.13.1")
+    val versions = mapOf(
+        "kotlinTest" to "1.3.72",
+        "rxJava" to "2.2.20",
+        "kotlinStdLib" to "1.3.72",
+        "kotlinCoroutinesCore" to "1.3.7",
+        "kotlinCoroutinesRx2" to "1.3.7",
+        "kotestAssertionsCore" to "4.2.2",
+        "kotestRunnerJunit5" to "4.2.2",
+        "junit" to "4.13.1"
+    )
+    val libs = mapOf(
+        "kotlinTest" to kotlin("test"),
+        "kotlinReflect" to "org.jetbrains.kotlin:kotlin-reflect",
+        "rxJava" to "io.reactivex.rxjava2:rxjava",
+        "kotlinStdLib" to "org.jetbrains.kotlin:kotlin-stdlib-jdk7",
+        "kotlinCoroutinesCore" to "org.jetbrains.kotlinx:kotlinx-coroutines-core",
+        "kotlinCoroutinesRx2" to "org.jetbrains.kotlinx:kotlinx-coroutines-rx2",
+        "kotestAssertionsCore" to "io.kotest:kotest-assertions-core",
+        "kotestRunnerJunit5" to "io.kotest:kotest-runner-junit5",
+        "junit" to "junit:junit"
+    )
+
+    testImplementation(libs["kotlinTest"]!!)
+    implementation("${libs["kotlinReflect"]}:${versions["kotlinTest"]}")
+    implementation("${libs["rxJava"]}:${versions["rxJava"]}")
+    implementation("${libs["kotlinStdLib"]}:${versions["kotlinStdLib"]}")
+    implementation("${libs["kotlinCoroutinesCore"]}:${versions["kotlinCoroutinesCore"]}")
+    implementation("${libs["kotlinCoroutinesRx2"]}:${versions["kotlinCoroutinesRx2"]}")
+    testImplementation("${libs["kotestAssertionsCore"]}:${versions["kotestAssertionsCore"]}")
+    testImplementation("${libs["kotestRunnerJunit5"]}:${versions["kotestRunnerJunit5"]}")
+    testImplementation("${libs["junit"]}:${versions["junit"]}")
+    implementation("io.gitlab.arturbosch.detekt:detekt-cli:1.12.0")
 }
 
 tasks.test {
@@ -31,6 +57,20 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+detekt {
+    input = files("src/main/kotlin")
+    reports {
+        xml.enabled = true
+        html.enabled = false
+        txt.enabled = false
+    }
+}
+
+tasks.withType<Detekt> {
+    group = "verification"
+    description = "Run static code analysis with detekt"
 }
 
 application {
--- src/test/kotlin/sort/BogoSortTest.kt
+++ src/test/kotlin/sort/BogoSortTest.kt
@@ -2,6 +2,9 @@ package sort
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.jvm.isAccessible
 
 class BogoSortTest : StringSpec({
 
@@ -53,20 +56,32 @@ class BogoSortTest : StringSpec({
         val actual = BogoSort.sort(arr)
         actual shouldBe expected
     }
-    //because this algorithm time complexity is O(n*n!) I commented next lines for faster testing purpose
-/*
-    "BogoSort should sort an array of size 15" {
-        val arr = intArrayOf(15, 2, 5, 7, 4, 3, 9, 8, 1, 6, 10, 11, 13, 14, 12)
-        val expected = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-        val actual = BogoSort.sort(arr)
-        actual shouldBe expected
+
+    "Bogo sort should sort an array" {
+        val arr = intArrayOf(3, 5, 2, 7, 1)
+        val expected = intArrayOf(1, 2, 3, 5, 7)
+
+        BogoSort.sort(arr) shouldBe expected
     }
 
-    "BogoSort should sort an array of size 20" {
-        val arr = intArrayOf(20, 2, 5, 7, 4, 3, 9, 8, 1, 6, 10, 11, 13, 14, 12, 15, 16, 19, 18, 17)
-        val expected = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-        val actual = BogoSort.sort(arr)
-        actual shouldBe expected
+    "isSorted should check if array is sorted" {
+        val sortedArray = intArrayOf(1, 2, 3, 5, 7)
+        val unsortedArray = intArrayOf(3, 5, 2, 7, 1)
+
+        BogoSort::class.memberFunctions.find { it.name == "isSorted" }?.also { it.isAccessible = true }
+            ?.call(BogoSort, sortedArray) shouldBe true
+        BogoSort::class.memberFunctions.find { it.name == "isSorted" }?.also { it.isAccessible = true }
+            ?.call(BogoSort, unsortedArray) shouldBe false
     }
-*/
+
+    "shuffle should randomize the elements of an array" {
+        val arr = intArrayOf(1, 2, 3, 4, 5)
+
+        BogoSort::class.memberFunctions.find { it.name == "shuffle" }?.also {
+            it.isAccessible = true
+        }?.call(BogoSort, arr)
+
+        arr shouldNotBe intArrayOf(1, 2, 3, 4, 5)
+    }
+
 })
